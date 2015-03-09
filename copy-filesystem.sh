e452156430799b24a8415e7cc654b8fe14396bcf
Christopher Horrell <christopher@joyent.com>
2015-03-09 17:08:19 -0400

    typo

--- copy-filesystem.sh
+++ copy-filesystem.sh
@@ -6,6 +6,6 @@ IFS=$'\n\t'
 BUILD_DATE=`date +%Y%m%d`
 
 # Tars up the required system files and dirctories.
-EXCLUDE="-exclude dev --exclude sys --exclude proc --exclude tmp"
+EXCLUDE="--exclude dev --exclude sys --exclude proc --exclude tmp"
 cd /
 tar czf /tmp/centos-7-${BUILD_DATE}.tar.gz ${EXCLUDE} .
