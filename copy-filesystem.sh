78bd846e6621efe45042023f687c665a7ab8a43c
Christopher Horrell <christopher@joyent.com>
2015-03-09 17:33:49 -0400

    Fixed exclude patterns

--- copy-filesystem.sh
+++ copy-filesystem.sh
@@ -6,5 +6,4 @@ IFS=$'\n\t'
 BUILD_DATE=`date +%Y%m%d`
 
 # Tars up the required system files and dirctories.
-cd /
-tar czf /tmp/centos-7-${BUILD_DATE}.tar.gz -X exclude.txt .
+tar czf /tmp/centos-7-${BUILD_DATE}.tar.gz --exclude-from=exclude.txt /
