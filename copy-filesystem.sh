2d48dc2709847aba007d9d587da71e30a8eabac6
Christopher Horrell <christopher@joyent.com>
2015-03-09 20:10:58 -0400

    Don't save the tar file to tmp

--- copy-filesystem.sh
+++ copy-filesystem.sh
@@ -6,4 +6,4 @@ IFS=$'\n\t'
 BUILD_DATE=`date +%Y%m%d`
 
 # Tars up the required system files and dirctories.
-tar czf /tmp/centos-7-${BUILD_DATE}.tar.gz --exclude-from=exclude.txt /
+tar czf ./centos-7-${BUILD_DATE}.tar.gz --exclude-from=exclude.txt /
